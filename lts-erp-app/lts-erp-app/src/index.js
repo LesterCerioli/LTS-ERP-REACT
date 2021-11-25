88d851993040221669206122d90fd44435e87da1
LesterCerioli <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-11-25 19:37:38 -0300

    Implementing react structure

--- lts-erp-app/lts-erp-app/src/index.js
+++ lts-erp-app/lts-erp-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from '../src/views/login/';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
